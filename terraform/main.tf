20f2ce1b6077e97b0f8683e637666e084f408099
Sam Kasimalla <skasimalla@Samuels-MacBook-Pro.local>
2023-01-05 22:00:04 -0500

    Updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,25 @@
-terraform {
 
+/*terraform {
   backend "s3" {
-    bucket  = "coworks-terraform-state-files"
+    bucket  = "terraform-state-files"
     key     = "dev/terraform2.tfstate"
     region  = "us-east-1"
-    profile = "coworks"
+    profile = "lab1"
   }
 }
-
+*/
 
 provider "aws" {
   region  = "us-east-1"
-  profile = "coworks"
+  profile = "lab1"
 
 }
-
 
 resource "aws_instance" "web" {
 
   ami           = "ami-0022f774911c1d690"
   instance_type = "t2.micro"
-  key_name      = "sam-coworks"
+  key_name      = "sam-lab1"
 
   tags = {
     Name = "petclinic-ec2-tf"
